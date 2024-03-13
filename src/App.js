979090dcbee3925d95d9c0a29b95139daec8e850
Jose Albero Coca <ccalbertj7@gmail.com.com>
2024-03-13 15:16:23 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// app.js
+import React from 'react';
 
 function App() {
+  // Función para redirigir al enlace externo
+  const redirectToExternalLink = () => {
+    window.location.href = "https://sites.google.com/uptc.edu.co/avaecosistemas/inicio";
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{textAlign: 'center', backgroundColor: '#001f3f', color: 'white', fontFamily: 'Cousin', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1 style={{fontSize: '3rem'}}>Aprende sobre ecosistemas en Colombia</h1>
+        <p style={{fontSize: '1.5rem'}}>Bienvenido a nuestra plataforma de aprendizaje sobre los maravillosos ecosistemas colombianos.</p>
+        {/* Utiliza un elemento <a> con el atributo href para redirigir */}
+        <a href="https://sites.google.com/uptc.edu.co/avaecosistemas/inicio" target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none'}}>
+          <button style={{padding: '15px 30px', fontSize: '1.5rem', backgroundColor: '#ff5733', border: 'none', borderRadius: '10px', cursor: 'pointer', boxShadow: '0 4px 8px rgba(0,0,0,0.3)', transition: 'background-color 0.3s, transform 0.3s'}}>
+            Iniciar
+          </button>
         </a>
+        <div style={{marginTop: '20px'}}>
+          <p style={{fontSize: '1.2rem', marginTop: '10px'}}>¡Diviértete aprendiendo!</p>
+        </div>
       </header>
     </div>
   );
